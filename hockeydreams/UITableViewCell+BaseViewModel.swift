48227398e38f93f667496173c56c132814b18ba4
Ruben Alonso <ruben.alons@gmail.com>
2016-10-09 23:22:17 +0200

    add table view cell extensions

---
+++ hockeydreams/UITableViewCell+BaseViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+BaseViewModel.swift
+//  hockeydreams
+//
+//  Created by Ruben Alonso on 9/10/16.
+//  Copyright © 2016 boomstick. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell: BaseCell {
+    func set(viewModel: Any) {
+        
+    }
+}
---
+++ hockeydreams/UITableViewCell+Reusable.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+Reusable.swift
+//  hockeydreams
+//
+//  Created by Ruben Alonso on 9/10/16.
+//  Copyright © 2016 boomstick. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell: Reusable {
+    
+}
