aa72db39a6b66dfbaf3f09896b77df8e6e6b93ef
Ruben Alonso <ruben.alons@gmail.com>
2016-10-09 23:17:13 +0200

    add app token entity

---
+++ hockeydreams/AppToken.swift
@@ -0,0 +1,14 @@
+//
+//  AppToken.swift
+//  hockeydreams
+//
+//  Created by Ruben Alonso on 9/10/16.
+//  Copyright © 2016 boomstick. All rights reserved.
+//
+
+import Foundation
+
+struct AppToken {
+    var title = ""
+    var token = ""
+}
